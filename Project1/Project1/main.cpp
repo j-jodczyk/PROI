8e530c0ba4e261a765d83d231c0d955441cd2986
Julia Jodczyk <01158711@pw.edu.pl>
2021-03-12 11:39:00 +0100

    fixed comments

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -11,8 +11,8 @@ int main()
 
 	Player player1;
 
-	// player1.age; - C2248 error pr�ba dost�pu do prywatnego atrybutu klasy Player
-	// b��d wykryty podczas kompilacji, wi�c program si� nie uruchomi
+	// player1.age; - C2248 error proba dostepu do prywatnego atrybutu klasy Player
+	// blad wykryty podczas kompilacji, wiec program sie nie uruchomi
 
 	player1.setAge(26);
 	player1.setNumber(13);
@@ -22,7 +22,7 @@ int main()
 	player2.info();
 
 	// x = 5;  C2065 error undelared identifier; niezadeklarowany identyfikator,
-	// b��d wykryty podczas kompilacji
+	// blad wykryty podczas kompilacji
 
 	/*
 	*char finished = 'y';
@@ -31,8 +31,8 @@ int main()
 	*	std::cout << "continue? (y/n)" << std::endl;
 	*	std::cin >> finished;
 	*}
-	* C6282 warning, przypisanie sta�ej w kontek�cie Boolean, program si� uruchomi,
-	* ale p�tla b�dzie wykonywana w niesko�czono��
+	* C6282 warning, przypisanie stalej w kontekscie Boolean, program sie uruchomi,
+	* ale petla bedzie wykonywana w nieskonczonosc
 	*/
 
 	float bank_balance{ FLT_MAX };
