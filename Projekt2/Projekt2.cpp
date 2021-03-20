df4c73949907f80fc9910249c1913b1fb8a855d7
Julia Jodczyk <01158711@pw.edu.pl>
2021-03-20 20:13:27 +0100

    written drawChart, biggestInV and writeDeflection funct;

--- Projekt2/Projekt2.cpp
+++ Projekt2/Projekt2.cpp
@@ -7,58 +7,98 @@
 #include "CarData.h"
 
 
-std::string Abbrev(CarData cd);
+void writeDeflection(std::string filename, Chart c);
+void drawChart(Chart c);
+int biggestInV(std::vector<int> v);
 
-int averagePrice(Chart c);
-
-
-int main()
+int main(int argc, char *argv[])
 {
+    std::string filename = argv[1];
+    int pos = filename.length() - 4;
+    filename.insert(pos, 1, 'o');
     
-    Chart c{ { "Toyota  Aygo", 34900, {4, 5, 6, 7, 8, 9, 12, 2, 3, 6, 8, 9} }, {"Toyota  Aygo", 34900, {4, 5, 6, 7, 8, 9, 12, 2, 3, 6, 8, 9} } };
+    Chart c{ { "Toyota  Aygo", 34900, {4, 5, 6, 7, 8, 9, 12, 2, 3, 6, 8, 9} }, 
+        {"Opel  Adam", 43300, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }} };
     
-    std::cout << Abbrev(c[0]) << std::endl;
-
-    std::cout << averagePrice(c);
+    //std::cout << Abbrev(c[0]) << std::endl;
+    writeDeflection(filename, c);
+    //std::cout << averagePrice(c);
+    
+    drawChart(c);
+    
 
     return 0;
 }
 
 
-std::string Abbrev(CarData cd)
+void writeDeflection(std::string filename, Chart c)
 {
-    std::string abbrev;
-    abbrev += cd.brandVersion.substr(0, 2);
-    for (int i = 0; i < cd.brandVersion.length(); ++i)
-    {
-        if (cd.brandVersion[i] == ' ')
-        {
-            abbrev += cd.brandVersion.substr(i + 2, 2);
-            break;
-        }
-    }
-
-    for (int i = 0; i < abbrev.length(); ++i)
-    {
-        abbrev[i] = toupper(abbrev[i]);
-    }
-    return abbrev;
-}
-
-int averagePrice(Chart c)
-{
-    int avarage{ 0 };
-    int sum{ 0 };
-    int count{ 0 };
-
+    std::ofstream MyFile(filename);
     for (int i = 0; i < 20; ++i)
     {
-        if (c[i].brandVersion != "\0")
-        {
-            sum += c[i].price;
-            count++;
-        }
+        if (c[i].brandVersion == "\0")
+            break;
+        MyFile << Abbrev(c[i]) << " : " << c[i].price - averagePrice(c) << std::endl;
     }
-    avarage = sum / count;
-    return avarage;
+    MyFile.close();
+}
+
+
+void drawChart(Chart c)
+{
+    std::vector<int> carsSold;
+    for (int i = 0; i < 12; i++)
+    {
+        int monthlySold = 0;
+        for (int j = 0; j < 20; j++)
+        {
+            if (c[j].brandVersion == "\0")
+                break;
+            monthlySold += c[j].soldMonth[i];
+        }
+        carsSold.push_back(monthlySold);
+    }
+
+    std::cout << "s l m k m c l s w p l g" <<std::endl;
+
+    for (int i = 0; i < carsSold.size(); ++i)
+        std::cout << carsSold[i] << ' ';
+    std::cout << std::endl;
+    int size = biggestInV(carsSold);
+    for (int i = 0; i < size; ++i)
+    {
+        char arr[12];
+        for (int j = 0; j < 12; ++j)
+        {   
+            if (carsSold[j] != 0)
+            {
+                arr[j] = '#';
+                carsSold[j] -= 1;
+            }
+            else
+                arr[j] = ' ';
+        }
+        for (int z = 0; z < 12; ++z)
+            std::cout << arr[z] << ' ';
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < carsSold.size(); ++i)
+        std::cout << carsSold[i] << ' ';
+    std::cout << std::endl;
+
+    
+}
+
+
+int biggestInV(std::vector<int> v)
+{
+    int biggest = 0;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (v[i] > biggest)
+            biggest = v[i];
+    }
+
+    return biggest;
 }
