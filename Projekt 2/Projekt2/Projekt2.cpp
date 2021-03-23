5b3c6888e2fcd445fca9e7d7f0af0e5834f600c6
Julia Jodczyk <01158711@pw.edu.pl>
2021-03-23 15:29:16 +0100

    written newFileName function, creating file names with 'o' added at the end

--- Projekt 2/Projekt2/Projekt2.cpp
+++ Projekt 2/Projekt2/Projekt2.cpp
@@ -7,13 +7,12 @@
 #include "CarData.h"
 
 
+std::string newFileName(std::string filename);
 
 
 int main(int argc, char *argv[])
 {
-    std::string filename = argv[1];
-    int pos = filename.length() - 4;
-    filename.insert(pos, 1, 'o');
+    std::string filename = newFileName(argv[1]);
     Chart s;
     std::ifstream file(argv[1]);
     file >> s;
@@ -22,4 +21,18 @@ int main(int argc, char *argv[])
     writeDeflection(filename, s);
 
     return 0;
+}
+
+
+std::string newFileName(std::string filename)
+{
+    int pos = 0;
+    for (int i = 0; i < filename.length(); ++i)
+    {
+        if (filename[i] == '.')
+            break;
+        pos++;
+    }
+    filename.insert(pos, 1, 'o');
+    return filename;
 }
